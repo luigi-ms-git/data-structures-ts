cb69e5bfefe9bc1b9def1bfbcc57b661257147a4
Luigi Moraes <luigimoraes3@gmail.com>
2022-11-18 22:28:35 -0300

    added remove last feature to Heap. Heap finished

--- non-linear/Heap.ts
+++ non-linear/Heap.ts
@@ -1,51 +1,47 @@
 class HeapNode {
-  private _key: number;
+  public key: number;
+  public parentKey: number;
   private _data: any;
-  private _parentKey: number;
-  private _children: Array<HeapNode>;
-  public _MAX_CHILDREN: number;
+  private _nodes: Array<HeapNode>;
+  private _MAX_CHILDREN: number;
+  private _MIN_CHILDREN: number;
 
   constructor(data: any){
-    this._key = 0;
+    this.key = 0;
+    this.parentKey = 0;
     this._data = data;
-    this._parentKey = 0;
-    this._children = [];
+    this._nodes = [];
     this._MAX_CHILDREN = 2;
+    this._MIN_CHILDREN = 0;
   }
 
   public getData(): string { 
-    return `{ key: ${this.key}, data: ${this.data}, ${this.isLeaf ? `isLeaf: true` : `children: ${this.children}`} }`;
+    return `{ key: ${this.key}, data: ${this.data}, ${(this.isLeaf) ? "isLeaf: true" : `nodes: ${JSON.stringify(this.nodes.map((n: HeapNode) => n.getData()))}`} }`;
   }
-
-  public get key(): number { return this._key; }
-
-  public set key(nKey: number){ this._key = nKey; }
 
   public get data(): any { return this._data; }
 
-  public get parentKey(): number { return this._parentKey; }
-
-  public set parentKey(nParentKey: number){ this._parentKey = nParentKey; }
-
-  public get children(): Array<HeapNode> { return this._children; }
+  //the left and right nodes are kept in the nodes array
+  public get nodes(): Array<HeapNode> { return this._nodes; }
 
   public get MAX_CHILDREN(): number { return this._MAX_CHILDREN; }
 
-  public get isLeaf(): boolean { return this.children.length > 0; }
+  public get hasBothNodes(): boolean { return this.nodes.length === this._MAX_CHILDREN; }
 
-  public get hasMaxChildren(): boolean { return this.children.length == this.MAX_CHILDREN; }
+  public get isLeaf(): boolean { return this.nodes.length === this._MIN_CHILDREN; }
 }
 
 class Heap {
   private _root: HeapNode;
-  private _struct: Array<HeapNode>;
+  private _stack: Array<HeapNode>;
 
   constructor(maxValue: number){
     this._root = new HeapNode("ROOT");
     this._root.key = maxValue;
-    this._struct = [this._root];
+    this._stack = [this._root];
   }
 
+  //no parentKey passed means you want to add the node to Root
   public insert(data: any, key: number, parentKey?: number): void {
     if(key < this._root.key){
       const newNode = new HeapNode(data);
@@ -55,50 +51,53 @@ class Heap {
         const p = this.getNode(parentKey);
         newNode.parentKey = parentKey;
  
-        if(!p.hasMaxChildren){
-          p.children.push(newNode);
-          this._struct[this.getNodeIndex(parentKey)] = p; 
-          this._struct.push(newNode);
+        if(!p.hasBothNodes){
+          p.nodes.push(newNode);
+          this._stack[this.getNodeIndex(parentKey)] = p; 
+          this._stack.push(newNode);
         }
-      }else if(!this._root.hasMaxChildren){
+      }else if(!this._root.hasBothNodes){
         newNode.parentKey = this._root.key;
         
-        this._root.children.push(newNode); 
-        this._struct.push(newNode);
+        this._root.nodes.push(newNode); 
+        this._stack.push(newNode);
       }
     }
   }
 
-  public remove(key: number, parentKey?: number): void {
+  //no parentKey passed means you want to remove the node from Root
+  public removeLast(key: number, parentKey?: number): void {
     if(this.nodeExists(key) && this._root.key !== key){
-      const node = this.getNode(key),
-          nodeIndex = this.getNodeIndex(key);
+      const node = this.getNode(key);
+      let nodeIndex = this.getNodeIndex(key);
 
-      if(!node.isLeaf){
-        //Remove children from struct and 
-        //remove node from both struct and parent
-      }else if(parentKey){
+      this._stack.splice(nodeIndex, 1);
+
+      if(parentKey){
         const parentNode = this.getNode(parentKey);
+        nodeIndex = this.getNodeIndex(key, parentNode.nodes);
 
-        parentNode.children.splice(nodeIndex, 1);
-        this._struct.splice(nodeIndex, 1);
-        this._struct.splice(parentKey, 1, parentNode);
+        parentNode.nodes.splice(nodeIndex, 1);
+
+        this._stack[this.getNodeIndex(parentKey)] = parentNode;
       }else{
-        const rootChildIndex = this._root.children.findIndex(n => n.key === nodeIndex);
+        const rootChildIndex = this._root.nodes.findIndex(n => n.key === nodeIndex);
 
-        this._struct.splice(nodeIndex, 1);
-        this._root.children.splice(rootChildIndex, 1);
+        this._root.nodes.splice(rootChildIndex, 1);
       }
     }
   }
 
   public getNode(key: number): HeapNode {
-    const found = this._struct.find(n => n.key === key);
+    const found = this._stack.find(n => n.key === key);
     return (found !== undefined) ? found : new HeapNode(null); 
   }
 
-  private getNodeIndex(key: number): number {
-    const index = this._struct.findIndex(n => n.key === key);
+  private getNodeIndex(key: number, arr?: Array<HeapNode>): number {
+    const index = (arr)
+      ? arr.findIndex(n => n.key === key)
+      : this._stack.findIndex(n => n.key === key);
+
     return (index !== undefined) ? index : 0; 
   }
 
@@ -107,22 +106,6 @@ class Heap {
   }
 
   public get nodes(): Array<string> { 
-    return this._struct.map(n => n.getData());
+    return this._stack.map(n => n.getData());
   }
 }
-
-const h = new Heap(8);
-
-console.log(h.nodes);
-h.insert("hello", 7);
-
-console.log(h.nodes);
-h.insert("ola", 6);
-
-console.log(h.nodes);
-h.insert("hallo", 5, 7);
-
-console.log(h.nodes);
-h.insert("hola", 4, 7);
-
-console.log(h.nodes);
